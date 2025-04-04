edc895d3dfa1edd7537d9d4322b70f5b94290c81
win <mr.vjcspy@gmail.com>
2025-04-04 15:24:58 +0700

    chore

--- Assets/Scripts/ND25/Core/ReactiveMachine/ReactiveMachineState.cs
+++ Assets/Scripts/ND25/Core/ReactiveMachine/ReactiveMachineState.cs
@@ -1,12 +1,10 @@
 ﻿using System.Collections.Generic;
-using UnityEngine;
-
 namespace ND25.Core.ReactiveMachine
 {
     public class ReactiveMachineState
     {
-        private readonly ReactiveMachineStateConfig config;
-        private readonly ReactiveMachine machine;
+        readonly ReactiveMachineStateConfig config;
+        readonly ReactiveMachine machine;
 
         public ReactiveMachineState(ReactiveMachine machine, ReactiveMachineStateConfig config)
         {
@@ -23,48 +21,38 @@ namespace ND25.Core.ReactiveMachine
 
         public void Entry()
         {
-#if UNITY_EDITOR
-            Debug.Log("[FSM State] Entry");
-#endif
-
-            foreach (var action in config.entry) machine.DispatchAction(action);
-
+            foreach (ReactiveMachineAction action in config.entry)
+            {
+                machine.DispatchAction(action);
+            }
         }
 
 
         public void Invoke()
         {
-            foreach (ReactiveMachineAction action in config.invoke) machine.DispatchAction(action);
+            foreach (ReactiveMachineAction action in config.invoke)
+            {
+                machine.DispatchAction(action);
+            }
         }
 
         public void Exit()
         {
-#if UNITY_EDITOR
-            Debug.Log("[FSM State] Exit");
-#endif
-
-            foreach (var action in config.exit) machine.DispatchAction(action);
+            foreach (ReactiveMachineAction action in config.exit)
+            {
+                machine.DispatchAction(action);
+            }
         }
 
-        /// <summary>
-        ///     Khi nhận event từ bên ngoài dispatch vào FSM.
-        /// </summary>
-        /// <param name="eventName">Tên event như "jump", "move"...</param>
         public void DispatchEvent(string eventName)
         {
-#if UNITY_EDITOR
-            Debug.Log($"[FSM State] Handling Event: {eventName}");
-#endif
-
             if (!config.on.TryGetValue(eventName, out var transitions) || transitions == null || transitions.Count == 0)
+            {
                 return;
+            }
 
             // Hiện tại chỉ lấy transition đầu tiên nếu có nhiều lựa chọn.
-            var transition = transitions[0];
-
-#if UNITY_EDITOR
-            Debug.Log($"[FSM State] Event '{eventName}' matched. Transition to '{transition.target}'");
-#endif
+            StateTransition transition = transitions[0];
 
             machine.TransitionTo(transition.target, transition.actions);
         }
