dd240b8cafd3269e6af44de745a12255f0910554
win <mr.vjcspy@gmail.com>
2025-04-13 16:51:14 +0700

    chore

--- Assets/Scripts/ND25/Gameplay/Character/Entity/EntityXDirection.cs
+++ Assets/Scripts/ND25/Gameplay/Character/Entity/EntityXDirection.cs
@@ -1,18 +1,8 @@
 ﻿using ND25.Util.Common.Enum;
-using Unity.Entities;
 namespace ND25.Gameplay.Character.Entity
 {
     public interface EntityXDirection
     {
         public XDirection GetCurrentFacingDirection();
-    }
-
-    public class EnemyInfo
-    {
-        public int health;
-    }
-
-    public struct EnemyComponent : IComponentData
-    {
     }
 }
